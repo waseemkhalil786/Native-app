528bc3b6d5a0dd5e7ba4d15f3bc68eed9f80e4d6
Waseem Khalil <waseemkhalilworld@gmail.com>
2024-11-22 19:31:06 -0800

    native practice

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,20 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { Button, Image, Text, View } from "react-native";
+import { FlipInEasyX } from "react-native-reanimated";
+import { MaterialIcons } from "@expo/vector-icons";
+import { green } from "react-native-reanimated/lib/typescript/Colors";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -14,7 +22,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -28,12 +36,25 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <View style={{  flex: 1 }}>
+      <View
+        style={{
+          height: 50,
+          borderBottomColor: "#ccc",
+          borderBottomWidth: 1,
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          paddingHorizontal: 16,
+        }}
+      >
+        <MaterialIcons name="menu" size={24} color="green" />
+        <Text style={{fontSize:20 , color:'green', fontWeight:"700" }}>BYKEA by WK</Text>
+        <MaterialIcons name="add-call" size={24} color="green" />
+      </View>
+      <View>
+        <Image source={{uri:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn6vOkcKd3I3LMNPJ_skgtcWEkE1xJMAxMGw&s" }} />
+      </View>
+    </View>
   );
 }
