21db5078942309ed5e1d7073e3c841c85b0c3ba8
Waseem Khalil <waseemkhalilworld@gmail.com>
2024-11-25 19:43:08 -0800

    working on Ui

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,20 +4,18 @@ import {
   ThemeProvider,
 } from "@react-navigation/native";
 import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import "react-native-reanimated";
-
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { useColorScheme } from "@/hooks/useColorScheme";
-import { Button, Image, Text, View } from "react-native";
-import { FlipInEasyX } from "react-native-reanimated";
-import { MaterialIcons } from "@expo/vector-icons";
-import { green } from "react-native-reanimated/lib/typescript/Colors";
+import React from "react";
+import { Text, View, StyleSheet, Dimensions, Image } from "react-native";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
+
+const SCREEN_HEIGHT = Dimensions.get("screen").height;
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -36,25 +34,122 @@ export default function RootLayout() {
   }
 
   return (
-    <View style={{  flex: 1 }}>
-      <View
-        style={{
-          height: 50,
-          borderBottomColor: "#ccc",
-          borderBottomWidth: 1,
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-between",
-          paddingHorizontal: 16,
-        }}
-      >
+    <View style={styles.maincontainer}>
+      {/* header */}
+      <View style={styles.header}>
         <MaterialIcons name="menu" size={24} color="green" />
-        <Text style={{fontSize:20 , color:'green', fontWeight:"700" }}>BYKEA by WK</Text>
+        <Text style={styles.headerfont}>BYKEA</Text>
         <MaterialIcons name="add-call" size={24} color="green" />
       </View>
-      <View>
-        <Image source={{uri:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn6vOkcKd3I3LMNPJ_skgtcWEkE1xJMAxMGw&s" }} />
+
+      {/* banner img */}
+
+      <View style={styles.banner}>
+        <Image
+          style={styles.bannerImg}
+          source={{
+            uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZlehOF0EnLGJDTPc3TrBz93V25TIul_sNrQ&s",
+          }}
+        />
+      </View>
+
+      {/* info View */}
+
+      <View style={styles.infoView}>
+        <MaterialIcons name="attach-money" size={24} color="green" />
+        <MaterialIcons name="message" size={24} color="green" />
+      </View>
+
+      {/* bottom maincontainer */}
+
+      <View style={styles.bottomContainer}>
+        <View style={styles.row}>
+          <Card title={"Carpool"} icon={"electric-car"} bgColor={"#c1e1c5"} />
+          <Card title={"Ride"} icon={"bike-scooter"} bgColor={"#bedadc"} />
+        </View>
+        <View style={styles.row}>
+          <Card title={"Carpool"} icon={"electric-car"} bgColor={"#c1e1c5"} />
+          <Card title={"Ride"} icon={"bike-scooter"} bgColor={"#bedadc"} />
+        </View>
+        <View style={styles.row}>
+          <Card title={"Carpool"} icon={"electric-car"} bgColor={"#c1e1c5"} />
+          <Card title={"Ride"} icon={"bike-scooter"} bgColor={"#bedadc"} />
+        </View>
       </View>
     </View>
   );
 }
+
+const Card = ({
+  bgColor,
+  icon,
+  title,
+}: {
+  bgColor: string;
+  icon: any;
+  title: string;
+}) => {
+  return (
+    <View style={[styles.card, { backgroundColor: bgColor }]}>
+      <Text style={{ textAlign: "right" }}>{title}</Text>
+      <MaterialIcons name={icon} size={70} color="black" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  maincontainer: {
+    flex: 1,
+  },
+  headerfont: {
+    fontWeight: "bold",
+    letterSpacing: 3,
+    fontSize: 21,
+  },
+  header: {
+    height: 50,
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 16,
+  },
+  banner: {
+    height: SCREEN_HEIGHT / 3.7,
+    backgroundColor: "#DEE3E8",
+    paddingHorizontal: 20,
+    paddingTop: 10,
+  },
+  bannerImg: {
+    height: "86%",
+    borderRadius: 12,
+  },
+  infoView: {
+    height: 45,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    borderRadius: 12,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.18,
+    shadowRadius: 1.0,
+
+    elevation: 1,
+    marginTop: -15,
+    marginHorizontal: 20,
+  },
+  bottomContainer: { flex: 1, margin: 20, gap: 15 },
+  row: { flex: 1, flexDirection: "row", gap: 15 },
+  card: {
+    flex: 1,
+    borderRadius: 12,
+    padding: 10,
+    justifyContent: "space-between",
+  },
+});
